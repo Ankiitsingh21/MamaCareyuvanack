34974d97b25f16708e5aafe94f9f9bf4942ee2a2
Ankit <ankiitsingh21@gmail.com>
2023-02-23 23:59:50 +0530

    good

---
+++ app/src/main/java/com/example/mamacare/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mamacare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent ihome = new Intent(MainActivity.this,loginpage.class);
+                startActivity(ihome);
+            }
+        },3000);
+
+    }
+}
