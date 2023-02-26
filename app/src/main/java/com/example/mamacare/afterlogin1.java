400718579ff97b51348fc8a474078a0ab3ed95aa
Ankit <ankiitsingh21@gmail.com>
2023-02-26 18:51:59 +0530

    good

--- app/src/main/java/com/example/mamacare/afterlogin1.java
+++ app/src/main/java/com/example/mamacare/afterlogin1.java
@@ -1,14 +1,46 @@
 package com.example.mamacare;
 
+import static androidx.constraintlayout.motion.widget.TransitionBuilder.validate;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class afterlogin1 extends AppCompatActivity {
+    private Button button;
+    private EditText disease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afterlogin1);
+        button = (Button)findViewById(R.id.button);
+        disease = (EditText)findViewById(R.id.disease);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(disease.getText().toString());
+            }
+        });
+    };
+
+
+    private void validate(String mdisease)
+    {
+        if (mdisease == "cough")
+        {
+            Intent intent1 = new Intent(afterlogin1.this,fever.class);
+            startActivity(intent1);
+        }
+        else {
+            Intent intent2 =new Intent(afterlogin1.this,wirell.class);
+            startActivity(intent2);
+
+        }
     }
 }
--- app/src/main/java/com/example/mamacare/loginpage.java
+++ app/src/main/java/com/example/mamacare/loginpage.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 
 public class loginpage extends AppCompatActivity {
-     private View move;
+     private View login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
-        move=findViewById(R.id.login);
-        move.setOnClickListener(new View.OnClickListener() {
+        login=findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent ihome=new Intent(loginpage.this,afterlogin1.class);
                 startActivity(ihome);
             }
         });
-    };
+    }
 }
